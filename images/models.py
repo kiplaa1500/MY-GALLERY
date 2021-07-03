d942db7712bcb620378c16152b876aea4aefa866
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-03 16:29:19 +0300

    Having class image with column fields

--- images/models.py
+++ images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='articles/')
+    name = models.CharField(max_length=55)
+    author =models.CharField(max_length=255,default='Admin')
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
