e0802aebda6e83298eaf600d536402b8cfa65e47
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-04 00:55:14 +0300

    Adding views to get request and return the results involves filtering of db

--- images/views.py
+++ images/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Image,Location,Category
+from django.http import HttpResponse, Http404
+import datetime as dt
+
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'gallery.html')
+def my_gallery(request):
+    date = dt.date.today()
+    images = Image.objects.all()
+    return render(request, 'gallery.html', {"date": date,"images":images})
+
+def search_location(request):
+    if 'location' in request.GET and request.GET["location"]:
+        image_location = request.GET.get("location")
+        searched_images = Image.filter_by_location(image_location)
+        message = f"{image_location}"
+        print("Image.......",searched_images)
+        return render(request, 'location.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image location"
+        return render(request, 'location.html', {"message": message})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        return render(request, 'search.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'search.html', {"message": message})
