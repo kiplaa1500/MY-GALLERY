162fd5e4360087e7d307abdad70e82f40619767a
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-03 15:59:47 +0300

    Creating a database and adding the db names and user name with password in settings.py then run migration

--- gallery/settings.py
+++ gallery/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -125,3 +125,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+    'PASSWORD':'kiplangat18',
+    }
+}
