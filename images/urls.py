e1872cdc02c6b4fa21b5de2d4d26dc08daf00a0d
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-04 00:56:23 +0300

    Changing the url to return page as requested

--- images/urls.py
+++ images/urls.py
@@ -4,5 +4,7 @@ from . import views
 # setting the url patterns
 
 urlpatterns =[
-    url('^$', views.welcome, name='welcome'),
+    url(r'^$', views.my_gallery, name = 'myGallery'),
+    url(r'^search$', views.search_results, name='search_results'),
+    url(r'^search/location$', views.search_location, name='search_location'),
 ]
