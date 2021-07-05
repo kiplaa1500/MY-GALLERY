6a4c74969603afdd939d51549280403edd7598ce
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-05 12:16:00 +0300

    Deployment to heroku

---
+++ ReadMe.md
@@ -0,0 +1,57 @@
+# DenJal Gallery 
+#### By Dennis Kiplangat Cheruiyot 
+
+## Description
+DenJal is a Django based personal gallery. 
+Noteable features of the app include:
+1. Admin portal:
+Here, the app owner is required to login in by providing necessary credentials. Upon succesful login, they can then proceed to upload, update or delete an image. They can also create, delete and edit image loactions and categories.
+2. Image details:
+To view the image details, click on the respective image and a modal pops up. The modal has various features which are:
+- Modal header: This contains the image name
+- Modal body: This contains the magnified image, the category tag, image location, date and time the image was posted and last but not least, the image description
+3. Image filtering by category and location:
+- By category: To view images in the same category, one can search by category name in the search form located in the navbar or by clicking on the category tag located in the image modal
+- By location: To view images from the same location, the user can click on the search by location id name which is in the navabar.
+4. Image copy: 
+Spotted an image you'd love to copy? The application allows for image copying. To copy a specific image, click on the image to reveal the image modal pop up then click on the copy icon which copies the image link to your clipboard.
+
+## Live link
+Visit the application on .
+
+## Development
+To make advancements/modifications, follow these steps:
+
+- Fork the repository
+- Create a new branch (`git checkout -b improve-feature`)
+- Make the appropriate changes in the files
+- Add changes made
+- Commit your changes (`git commit -am 'Improve feature'`)
+- Push to the branch (`git push origin improve-feature`)
+- Create a Pull Request 
+
+## Setup & Run instructions
+- Create and activate a virtual environment
+- Install the dependencies listed in the `requirements.txt`
+- Create a `.env` file. This will contain environment variables as listed in the `.env.sample` file.
+- Finally, run your app on `MODE='dev'` config for debugging purposes
+
+## Technologies Used
+Technologies used to develop this application:
+
+1. Python v3
+2. Django 3
+3. Javascript
+4. Cloudinary
+5. Bootstrap
+6. HTML 
+7. CSS
+
+
+## Support and contact details
+
+Should you be unable to access the website, have any recommendations and/or questions, feel free to email me:[kiplangatdennis99@gtmail.com](mailto:kiplangatdennis99@gtmail.com)
+
+## [License]
+
+Copyright (c) 2021 [Dennis] 
--- gallery/settings.py
+++ gallery/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from decouple import config,Csv
+import dj_database_url
+import django_heroku
+
 
 cloudinary.config( 
   cloud_name = "dqvarhwzn", 
@@ -22,8 +26,39 @@ cloudinary.config(
   secure = True
 )
 
+MODE = config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+# development
+if config('MODE') == "dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -60,6 +95,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gallery.urls'
@@ -126,22 +162,28 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'moringa',
-    'PASSWORD':'kiplangat18',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gallery',
+#         'USER': 'moringa',
+#     'PASSWORD':'kiplangat18',
+#     }
+# }
+django_heroku.settings(locals())
